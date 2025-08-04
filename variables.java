91f4df45560827297967b131403d05ab76e14eeb
Krishnaprasath <230801093@rajalakshmi.edu.in>
2025-08-04 09:26:09 +0530

    first commit

---
+++ variables.java
@@ -0,0 +1,15 @@
+public class variables{
+    public static void main(String[] args) {
+        boolean ishungry = false;
+        
+
+
+        if(ishungry){
+            System.out.println("Eat dinner!");
+        }
+        else{
+            System.out.println("Lol skill issues!");
+        }
+
+    }
+}
