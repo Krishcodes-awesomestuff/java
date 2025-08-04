a057859ef313d8d7b25e2ec373baf05c82afd126
Krishnaprasath <230801093@rajalakshmi.edu.in>
2025-08-04 09:39:11 +0530

    Variables in Java

---
+++ Chp 2/Main.java
@@ -0,0 +1,21 @@
+// 2 Steps to creating a variable:
+// 1. Declaration
+// 2. Assignment
+
+
+public class Main{
+    public static void main(String[] args) {
+       // variable = a reusable container for a value
+       //            a variable behaves as if it was the value it contains
+       int age = 30; // age is a variable of type int, initialized to 30 
+       System.out.println("The variable has the number: " +age); // prints the value of age to the console              
+    }
+}
+
+// Data types in Java:
+// int - integer (whole number)
+// double - floating point number (decimal)
+// String - sequence of characters (text)
+// boolean - true or false value
+// char - single character
+
